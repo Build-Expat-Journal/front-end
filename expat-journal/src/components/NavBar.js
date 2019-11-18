dba50c5c95d7ba16ce3db3f63e1940546e041b9e
cstewart94 <shassey.mayhem@gmail.com>
2019-11-18 12:44:04 -0600

    started NavBar

--- expat-journal/src/components/NavBar.js
+++ expat-journal/src/components/NavBar.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    return (
+  return (
+    <>
+      <div>
         <h1>Expat Journal</h1>
-    )
+      </div>
+      <div><Link to='/'>Home</Link></div>
+      <div><Link to=''>New Trip</Link></div>
+      <div><Link to=''>Profile</Link></div>
+    </>
+  )
 };
 
 export default NavBar;
